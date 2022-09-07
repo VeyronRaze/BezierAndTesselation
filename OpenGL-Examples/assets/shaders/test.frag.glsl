0f0210a80b02c1681888f2381256e8f0242e37d2
VeyronRaze <veyronraze@gmail.com>
2022-09-08 02:05:07 +0300

    Well, tessellation, folks

--- OpenGL-Examples/assets/shaders/test.frag.glsl
+++ OpenGL-Examples/assets/shaders/test.frag.glsl
@@ -1,10 +1,19 @@
 #version 450 core
 
-layout (location = 0) out vec4 o_Color;
+out vec4 FragColor;
 
 uniform vec4 u_Color;
+uniform bool textured;
+in vec2 v_TexCoord;
+
+uniform sampler2D ourTexture;
 
 void main()
 {
-	o_Color = u_Color * vec4(1.0f);
+	if(textured)
+		FragColor = texture(ourTexture, v_TexCoord);
+	else
+		FragColor = u_Color * vec4(1.0f);
+
+//	FragColor = vec4(vec3(v_TexCoord.y), 1.0f);
 }
--- OpenGL-Examples/assets/shaders/test.vert.glsl
+++ OpenGL-Examples/assets/shaders/test.vert.glsl
@@ -1,10 +1,14 @@
 #version 450 core
 
-layout (location = 0) in vec3 a_Position;
+layout (location = 0) in vec2 a_Position;
+layout (location = 1) in vec2 texCoord;
+
+out vec2 v_TexCoord;
 
 uniform mat4 u_ViewProjection;
 
 void main()
 {
-	gl_Position = u_ViewProjection * vec4(a_Position, 1.0f);
+	v_TexCoord = texCoord;
+	gl_Position = u_ViewProjection * vec4(a_Position, 0.0f, 1.0f);
 }
